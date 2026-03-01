2e3310d04d0cef96a678c2a34769ba934689464b
Manuel Clavel <manuelgclavel@gmail.com>
2026-03-01 16:27:06 +0700

    working on it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mobile.campico"
         minSdk = 26  // originally 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1"
         //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner
     }
     buildFeatures {
